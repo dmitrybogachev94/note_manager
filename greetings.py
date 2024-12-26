9767392b294c799e7f5ad488943c4835ad7ed731
dmitrybogachev94 <dmitrybogachev94@icloud.com>
2024-12-26 21:14:23 +0300

    Grade 1. Этап 1: Задание 1

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Дмитрий"
+print(username)
+title = "заголовок заметки"
+print(title)
+countent = "описание заметки"
+print(countent)
+status = "статус заметки"
+print(status)
+created_date = "26-12-2024"
+print(created_date)
+issue_date = "26-01-2025"
+print(issue_date)
