cd07a564ce66747074246e430331ef616b3f6100
dmitrybogachev94 <dmitrybogachev94@icloud.com>
2025-01-16 20:56:45 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,12 @@
+username = "Дмитрий"
+print("Имя пользователя:",username)
+title = "Заметка"
+print("Заголовок заметки:", title)
+countent = "Содержание"
+print("Описание заметки:", countent)
+status = "Начал выполнение / Не начал выполнение"
+print("Статус заметки:", status)
+created_date = input("Введите дату создания заметки в формате ДД.ММ.ГГ:")
+print(created_date[0:5])
+issue_date= input("Введите дату завершения заметки в формате ДД.ММ.ГГ:")
+print(issue_date[0:5])
---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Дмитрий"
+print("Имя пользователя:",username)
+title = "Заметка"
+print("Заголовок заметки:", title)
+countent = "Содержание"
+print("Описание заметки:", countent)
+status = "Начал выполнение / Не начал выполнение"
+print("Статус заметки:", status)
+created_date = ("Дата создания заметки 12.12.24")
+print(created_date)
+issue_date= input("Дата завершения заметки в формате 12.01.25")
+print(issue_date)
+
